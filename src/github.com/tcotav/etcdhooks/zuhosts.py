0fea8610871769e16302c6dfdb1b0c41be10da81
tcotav <tcotav@gmail.com>
2015-09-08 14:47:49 -0700

    still f'ing around

--- src/github.com/tcotav/etcdhooks/zuhosts.py
+++ src/github.com/tcotav/etcdhooks/zuhosts.py
@@ -5,8 +5,9 @@ types=["web", "extapi", "papi"]
 hrange=[500, 549]
 
 domain="iad.prod.zulily.com"
-
+print "#!/bin/bash\n\n"
 for type in types:
   for hostnum in range(hrange[0], hrange[1]+1):
-    print "%s-%s-%s.%s" % (parent, type, hostnum, domain)
+    print "curl -L http://127.0.0.1:4001/v2/keys/%s/%s/%s -XPUT -d 0" % (parent, type, hostnum)
+
 
