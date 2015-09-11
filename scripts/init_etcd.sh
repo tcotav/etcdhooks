2518a73ba8d5b0319dcfb9b5c0b5a7ff0b3cf58c
james francis <jfrancis@zulily.com>
2015-09-11 14:30:54 -0700

    tuning the scripts

--- scripts/init_etcd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# simple script to dump some data into etcd to use for testing app
-curl -L http://127.0.0.1:4001/v2/keys/site/web/501 -XPUT -d value=1
-curl -L http://127.0.0.1:4001/v2/keys/site/web/502 -XPUT -d value=0
-curl -L http://127.0.0.1:4001/v2/keys/site/db/500 -XPUT -d value=1
---
+++ scripts/remove-hosts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "usage: script <start host ordinal> <end host ordinal>"
+  echo "./script 500 502" 
+  exit 1
+fi
+for hhost in web papi extapi; do
+  for hnum in {$1..$2}; do
+  curl -L http://127.0.0.1:4001/v2/keys/site/$hhost/$hnum -XDELETE
+  sleep 1
+done
+done
