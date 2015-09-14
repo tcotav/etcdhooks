761ecc24d65f6c7c4d31f1fb30e84057ba2c28ff
james francis <jfrancis@zulily.com>
2015-09-14 11:50:31 -0700

    l2 bash loop w/vars

--- scripts/add-hosts.sh
+++ scripts/add-hosts.sh
@@ -5,9 +5,10 @@ if [ $# -ne 2 ]; then
   echo "./script 500 502" 
   exit 1
 fi
+
 for hhost in web papi extapi; do
-  for hnum in {$1..$2}; do
-  curl -L http://127.0.0.1:4001/v2/keys/site/$hhost/$hnum -XDELETE
-  curl -L http://127.0.0.1:4001/v2/keys/site/$hhost/$hnum -XPUT -d value=0
+  for ((i=$1; i<=$2; i++)); do
+  curl -L http://127.0.0.1:4001/v2/keys/site/$hhost/$i -XPUT -d value=0
 done
 done
+
--- scripts/remove-hosts.sh
+++ scripts/remove-hosts.sh
@@ -5,9 +5,11 @@ if [ $# -ne 2 ]; then
   echo "./script 500 502" 
   exit 1
 fi
+
+
 for hhost in web papi extapi; do
-  for hnum in {$1..$2}; do
-  curl -L http://127.0.0.1:4001/v2/keys/site/$hhost/$hnum -XDELETE
-  sleep 1
-done
+
+  for ((i=$1; i<=$2; i++)); do
+    curl -L http://127.0.0.1:4001/v2/keys/site/$hhost/$i -XDELETE
+  done
 done
