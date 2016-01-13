2ec4b4ae60e96e29eb2a8dceaff48c43f99e69e0
JF <jfrancis@zulily.com>
2016-01-13 11:43:05 -0800

    full packaging script

--- scripts/createpkg.sh
+++ scripts/createpkg.sh
@@ -3,6 +3,7 @@
 work_dir=`pwd`
 package="etcdhooks"
 now=`date +%m%d%Y%H%M`
+REPO=/var/www/html/repo
 tgtdir="$package-$now"
 
 cd $GOPATH
@@ -13,6 +14,12 @@ mkdir -p $GOPATH/bin
 # build the binary
 go install github.com/tcotav/$package
 
+# test exit code
+if [ $? -ne 0 ]
+then
+  echo "build  of package $package failed"
+  exit 1
+fi
 
 # make deploy dir -- might exist
 mkdir -p $GOPATH/deploy/$tgtdir
@@ -31,3 +38,15 @@ tar -czvf $tgtdir.tar.gz $tgtdir
 rm -Rf $tgtdir
 
 shasum -a256 $tgtdir.tar.gz $tgtdir.tar.gz.sha  
+
+# now push to a web-accessible location to use with automation
+mv $tgtdir.tar.gz* $REPO
+
+# remove symlink
+rm $REPO/$package-current.tar.gz*
+
+# create new symlinks
+ln -s $REPO/$tgtdir.tar.gz $REPO/$package-current.tar.gz
+ln -s $REPO/$tgtdir.tar.gz.sha $REPO/$package-current.tar.gz.sha
+
+# run salt or whatever to pick it up -- report error state back to gitlab
